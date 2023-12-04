23252d685f43330ea31505140a7b500e1e7587a6
0xdante <cloudmelon74@gmail.com>
2023-12-04 12:50:44 +0300

    Add files via upload

---
+++ components/sub/HeroContent.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/utils/motion";
+import { SparklesIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+
+const HeroContent = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+    >
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">
+        Examine Chatbot
+          </h1>
+        </motion.div>
+
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+          Imagine with 
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              {" "}
+              Atlaspad Crypto {" "}
+            </span>
+            Launchpad {" "}
+            
+          </span>
+        </motion.div>
+
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+          Discover a vibrant community of creators on Serendipity! Connect with thousands of like-minded individuals on Discord or the web and unleash your imagination through collaborative storytelling. From vivid worlds to unforgettable characters, bring your short text descriptions to life in new and exciting ways. Join Serendipity today and embark on a journey of endless creativity!
+        </motion.p>
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+        >
+          Learn More!
+        </motion.a>
+      </div>
+
+      <motion.div
+        variants={slideInFromRight(0.9)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <Image
+          src="/atlas.svg"
+          alt="work icons"
+          height={850}
+          width={850}
+          className="w-full h-full flex justify-center items-center"
+        />
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default HeroContent;
---
+++ constants/index.ts
@@ -0,0 +1,56 @@
+export const Skill_data = [
+  ];
+  export const Socials = [
+
+  ];
+  export const Frontend_skill = [
+  ];
+
+  export const Backend_skill = [
+
+  ];
+
+  export const Full_stack = [
+    {
+      skill_name: "Tether",
+      Image: "/tether.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Avax",
+      Image: "/avax.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Dodge Coin",
+      Image: "/dodge.png",
+      width: 70,
+      height: 70,
+    },
+  
+    {
+      skill_name: "BNB",
+      Image: "/bnb.png",
+      width: 70,
+      height: 70,
+    },
+  
+  ];
+  
+  
+  
+  export const Other_skill = [
+    {
+      skill_name: "Ethereum",
+      Image: "/ethereum.png",
+      width: 50,
+      height: 50,
+    },
+  ];
+  
+  
+  
+  
+  
